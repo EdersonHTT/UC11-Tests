12a2ba63fa20669424eddf011de4c0ccba1cd9eb
EDERSON DA SILVA SCHMIDT <05194255020@senacrs.edu.br>
2026-04-28 21:47:59 -0300

    Aula 7

---
+++ Aula-7/src/controllers/UserController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import { UserService } from '../services/UserService';
+const userService = new UserService();
+
+export class UserController {
+  create(request: Request, response: Response): Response {
+    try {
+      const { name, email } = request.body;
+      const user = userService.createUser(name, email);
+      return response.status(201).json(user);
+    } catch (error) {
+      const message = error instanceof Error ? error.message : 'Erro interno';
+      return response.status(400).json({ error: message });
+    }
+  }
+
+  list(request: Request, response: Response): Response {
+    const users = userService.listUsers();
+    return response.status(200).json(users);
+  }
+
+  findById(request: Request, response: Response): Response {
+    const id = Number(request.params.id);
+    const user = userService.findUserById(id);
+    if (!user) {
+      return response.status(404).json({ error: 'Usuário não encontrado' });
+    }
+    return response.status(200).json(user);
+  }
+
+  findByEmail(request: Request, response: Response): Response {
+    const email:string = request.params.email.toString();
+    console.log(email)
+    const user = userService.findUserByEmail(email);
+    if (!user) {
+      return response.status(404).json({ error: 'Usuário não encontrado' });
+    }
+    return response.status(200).json(user);
+  }
+}
---
+++ Aula-7/src/services/UserService.ts
@@ -0,0 +1,41 @@
+import { error } from "console";
+import { User } from "../types/UserType";
+import { RetryAgent } from "undici-types";
+
+
+export class UserService {
+    private users: User[] = []
+    private currentId = 1;
+
+    createUser(name: string, email: string): User {
+        if(!name || !email) {
+            throw new Error( "Nome e email são obrigatórios")
+        }
+
+        const emailJaExiste = this.users.some((user) => user.email === email)
+        if(emailJaExiste) {
+            throw new Error("Email já cadastrado");
+        }
+
+        const newUser: User = {
+            id: this.currentId++,
+            name,
+            email,
+        }
+
+        this.users.push(newUser);
+        return newUser;
+    }
+
+    listUsers(): User[] {
+        return this.users;
+    }
+
+    findUserById(id: number): User | undefined {
+        return this.users.find((user) => user.id === id);
+    }
+
+    findUserByEmail(email: string): User | undefined {
+        return this.users.find((user) => user.email === email);
+    }
+}
